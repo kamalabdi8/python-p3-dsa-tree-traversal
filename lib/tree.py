c6dea90e55619a9da5101d496e82dc5131b5cb0e
kamalabdi8 <kamalabdi042@gmail.com>
2024-12-11 17:29:18 +0300

    initial commit

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,15 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, target_id):
+        def traverse(node):
+            if node['id'] == target_id:
+                return node
+            for child in node['children']:
+                result = traverse(child)
+                if result:
+                    return result
+            return None
+
+        return traverse(self.root)
